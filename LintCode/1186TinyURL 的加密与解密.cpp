b248c2e78bab61f0a722973848b78b237ee0acbc
Cang-cang Zhang <41459750+CmosZhang@users.noreply.github.com>
2019-12-11 15:43:17 +0800

    Add files via upload

---
+++ LintCode/1186TinyURL 的加密与解密.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+//1186. TinyURL 的加密与解密
+//
+
+unordered_map<string, string> tinyToLongUrl;
+hash<string> t;
+
+string encode(string &longUrl) {
+	// Encodes a URL to a shortened URL.
+	string key = to_string(t(longUrl));
+	tinyToLongUrl[key] = longUrl;
+	return key;
+}
+
+
+string decode(string shortUrl) {
+	// Decodes a shortened URL to its original URL.
+	return tinyToLongUrl[shortUrl];
+}
+
+
+int main() {
+	string str;
+	cin >> str;
+	cout << encode(str) << endl;
+	cout << decode(encode(str)) << endl;
+	system("pause");
+	return 0;
+}
