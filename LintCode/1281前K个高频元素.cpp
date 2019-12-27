a6625e82ce2aec8c7489961d369708f61896d013
Cang-cang Zhang <41459750+CmosZhang@users.noreply.github.com>
2019-12-27 20:20:57 +0800

    Add files via upload

---
+++ LintCode/1281前K个高频元素.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+
+using namespace std;
+
+//1281. 前K个高频元素
+//
+vector<int> topKFrequent(vector<int> &nums, int k) {
+	// Write your code here
+	if (nums.empty() || k >= nums.size()) {
+		return nums;
+	}
+	vector<int> res;
+
+}
