8e63658e98ad0275c185eb6751c13b7cbee42139
Cang-cang Zhang <41459750+CmosZhang@users.noreply.github.com>
2019-12-28 09:15:27 +0800

    Add files via upload

---
+++ LintCode/563背包问题 V.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<list>
+
+using namespace std;
+
+
+//563. ±≥∞¸Œ Ã‚ V
+//
+int backPackV(vector<int> &nums, int target) {
+	// write your code here
+	if (nums.empty()) {
+		return -1;
+	}
+	vector<int> dp(target + 1, 0);
+	dp[0] = 1;
+	for (int i = 1; i <= target; i++) {
+		dp[i] == (i == nums[0]) ? (dp[i] + 1) : 0;
+	}
+	for (int i = 1; i < nums.size(); i++) {
+		for (int j = target; j - nums[i] >= 0; j--) {
+			dp[j] += dp[j - nums[i]];
+		}
+	}
+	return dp[target];
+}
