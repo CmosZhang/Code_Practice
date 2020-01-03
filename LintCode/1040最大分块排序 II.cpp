e901d82396a83d4b6e48cbc305551a346eb1073a
Cang-cang Zhang <41459750+CmosZhang@users.noreply.github.com>
2020-01-03 16:05:47 +0800

    Add files via upload

--- LintCode/1040最大分块排序 II.cpp
+++ LintCode/1040最大分块排序 II.cpp
@@ -35,67 +35,69 @@ int maxChunksToSorted(vector<int> &arr) {
 	return res + 1;
 }
 
+//不能使用单调栈来做，这是有问题的，例如例子 arr={1,3,2,5,4,4,7,6,6,8}
 
 //方法二：
 //思路：单调栈，单调不减。栈顶总是保存当前遇到的最大的数，如果遇到一个比栈顶小的元素，则记录栈顶元素，
 //将栈中小于该元素的值全部弹出（意味着这些值应该分到一个块里面去）
 //最后将记录的栈顶值重新压回栈中，代表当前块中的最大值
 //您的提交打败了 77.66% 的提交!
-int maxChunksToSorted2(vector<int> &arr) {
-	if (arr.empty()) {
-		return -1;
-	}
-	int len = arr.size();
-	stack<int> stk;
-	stk.push(arr[0]);
-	for (int i = 1; i < len; i++) {
-		if (arr[i] >= stk.top()) {
-			stk.push(arr[i]);
-		}
-		else {
-			int temp = stk.top();
-			stk.pop();
-			while (!stk.empty() && stk.top() < temp) {
-				stk.pop();
-			}
-			stk.push(temp);
-		}
-	}
-	return stk.size();
-}
+//这个答案有问题啊
+//int maxChunksToSorted2(vector<int> &arr) {
+//	if (arr.empty()) {
+//		return -1;
+//	}
+//	int len = arr.size();
+//	stack<int> stk;
+//	stk.push(arr[0]);
+//	for (int i = 1; i < len; i++) {
+//		if (arr[i] >= stk.top()) {
+//			stk.push(arr[i]);
+//		}
+//		else {
+//			int temp = stk.top();
+//			stk.pop();
+//			while (!stk.empty() && stk.top() < temp) {
+//				stk.pop();
+//			}
+//			stk.push(temp);
+//		}
+//	}
+//	return stk.size();
+//}
 
-//通过40%的数据
-int maxChunksToSorted3(vector<int> &arr) {
-	// write your code here
-	if (arr.empty()) {
-		return -1;
-	}
-	int len = arr.size();
-	stack<int> stk;
-	stack<int> help;
-	stk.push(arr[0]);
-	for (int i = 1; i < len; i++) {
-		if (arr[i] > stk.top()) {
-			stk.push(arr[i]);
-		}
-		else {
-			int temp = stk.top();
-			stk.pop();
-			while (!stk.empty()) {
-				help.push(stk.top());
-				stk.pop();
-			}
-			while (!help.empty()) {
-				if (help.top() >= temp) {
-					stk.push(help.top());
-				}
-				help.pop();
-			}
-			stk.push(temp);
-		}
-	}
-	return stk.size();
-}
+////通过40%的数据
+//int maxChunksToSorted3(vector<int> &arr) {
+//	// write your code here
+//	if (arr.empty()) {
+//		return -1;
+//	}
+//	int len = arr.size();
+//	stack<int> stk;
+//	stack<int> help;
+//	stk.push(arr[0]);
+//	for (int i = 1; i < len; i++) {
+//		if (arr[i] > stk.top()) {
+//			stk.push(arr[i]);
+//		}
+//		else {
+//			int temp = stk.top();
+//			stk.pop();
+//			while (!stk.empty()) {
+//				help.push(stk.top());
+//				stk.pop();
+//			}
+//			while (!help.empty()) {
+//				if (help.top() >= temp) {
+//					stk.push(help.top());
+//				}
+//				help.pop();
+//			}
+//			stk.push(temp);
+//		}
+//	}
+//	return stk.size();
+//}
 
 
 
@@ -111,7 +113,7 @@ int main() {
 			break;
 		}
 	}
-	cout << maxChunksToSorted2(num) << endl;
+	cout << maxChunksToSorted(num) << endl;
 	system("pause");
 	return 0;
 }
